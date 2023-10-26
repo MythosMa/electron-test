f48664b322c7ebf472b8e33200306a06a8f1e6fc
MYTHOSMA-PC\马原 <mythosmayuan@gmail.com>
2023-10-26 18:01:39 +0800

    update

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain, dialog } = require('electron')
 const path = require('node:path')
 
+async function handleFileOpen () {
+  const { canceled, filePaths } = await dialog.showOpenDialog()
+  if (!canceled) {
+    return filePaths[0]
+  }
+}
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-        preload: path.join(__dirname, 'preload.js')
-      }
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  ipcMain.on("set-title", (event, title) => {
+    const webContents = event.sender;
+    const win = BrowserWindow.fromWebContents(webContents);
+    win.setTitle(title);
   });
 
   win.loadFile("index.html");
 };
 
 app.whenReady().then(() => {
+  ipcMain.handle("ping", () => "pong");
+  ipcMain.handle('dialog:openFile', handleFileOpen)
   createWindow();
 
   app.on("activate", () => {
---
+++ renderer.js
@@ -0,0 +1,27 @@
+const information = document.getElementById("info");
+information.innerText = `本应用正在使用 Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), 和 Electron (v${versions.electron()})`;
+
+const information2 = document.getElementById("hello");
+information2.innerText = test.hello();
+
+const setButton = document.getElementById('btn')
+const titleInput = document.getElementById('title')
+setButton.addEventListener('click', () => {
+  const title = titleInput.value
+  window.electronAPI.setTitle(title)
+})
+
+const fileBtn = document.getElementById('file-btn')
+const filePathElement = document.getElementById('filePath')
+
+fileBtn.addEventListener('click', async () => {
+  const filePath = await window.electronAPI.openFile()
+  filePathElement.innerText = filePath
+})
+
+const func = async () => {
+  const response = await window.versions.ping();
+  console.log(response); // 打印 'pong'
+};
+
+func();
